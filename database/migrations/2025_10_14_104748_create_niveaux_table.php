7ecedd70c80d8d77cb5c4582975790e83a572145
Rougss <rouguiatouba640@gmail.com>
2025-10-14 13:05:57 +0000

    Ajout des tables

---
+++ database/migrations/2025_10_14_104748_create_niveaux_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('niveaux', function (Blueprint $table) {
+            $table->id();
+             $table->string('intitule')->unique();
+              $table->foreignId('type_formations_id')->nullable() ->constrained('type_formations') ->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('niveaux');
+    }
+};
